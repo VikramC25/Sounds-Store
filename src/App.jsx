5cbac6aa96c1ec7e61ca52f823ef628dffcc8c2a
Vikram <vikramc2502@gmail.com>
2025-11-23 00:16:47 +0530

    Fix ThankYou page casing for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import PackPage from "./pages/PackPage";
-import Admin from "./pages/Admin";
-import ThankYou from "./pages/ThankYou";
-import CartDrawer from "./components/CartDrawer";
-import Privacy from "./pages/Privacy";
-import Terms from "./pages/Terms";
-import Refund from "./pages/Refund";
-import Shipping from "./pages/Shipping";
-import Contact from "./pages/Contact";
+  import { Routes, Route } from "react-router-dom";
+  import Home from "./pages/Home";
+  import PackPage from "./pages/PackPage";
+  import Admin from "./pages/Admin";
+  import ThankYou from "./pages/ThankYou";
+  import CartDrawer from "./components/CartDrawer";
+  import Privacy from "./pages/Privacy";
+  import Terms from "./pages/Terms";
+  import Refund from "./pages/Refund";
+  import Shipping from "./pages/Shipping";
+  import Contact from "./pages/Contact";
 
-function App() {
-  return (
-    <>
-      <CartDrawer />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pack/:slug" element={<PackPage />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/thank-you" element={<ThankYou />} />
-        
-        {/* Policy Routes */}
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/refund" element={<Refund />} />
-        <Route path="/shipping" element={<Shipping />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </>
-  );
-}
+  function App() {
+    return (
+      <>
+        <CartDrawer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pack/:slug" element={<PackPage />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/thank-you" element={<ThankYou />} />
+          
+          {/* Policy Routes */}
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/refund" element={<Refund />} />
+          <Route path="/shipping" element={<Shipping />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </>
+    );
+  }
 
-export default App;
+  export default App;
